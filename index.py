6fd55761bd6c4466e25ae6c369e1094bb3cb0141
falcon-90125 <sokolov_aa@ngs.ru>
2022-08-20 16:34:46 +0700

    Функция plural_form не закончено

--- index.py
+++ index.py
@@ -1,10 +1,35 @@
-"""Функция fizz_buzz"""
+# """Функция fizz_buzz"""
 
-def fizz_buzz(*args):
-    result = 0
-    for i in range(args[0], args[1]+1):
-        if i % 3 == 0 and i % 5 == 0:
-            result += i
-    return result
+# def fizz_buzz(*args):
+#     result = 0
+#     for i in range(args[0], args[1]+1):
+#         if i % 3 == 0 and i % 5 == 0:
+#             result += i
+#     return result
 
-print('1000-20000:', fizz_buzz(1000, 20000))
+# print('1000-20000:', fizz_buzz(1000, 20000))
+
+
+"""Функция plural_form"""
+i = 745
+noun1 = 'яблоко'
+noun2 = 'яблок'
+noun3 = 'яблока'
+
+def plural_form(i):
+    i = str(i)
+    i = int(i[-2:])
+
+    if i > 4 and i < 21:
+        noun = noun2
+    else:
+        i = str(i)
+        i = int(i[-1])
+        if i == 1:
+            noun = noun1
+        else:
+            noun = noun3
+
+    return noun
+
+print(plural_form(i))
